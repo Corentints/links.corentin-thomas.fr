f7da7ac6c3390812ec9c020aebfff625f8af0ac0
Corentin THOMAS <corentin.thomas2@etu.univ-lorraine.fr>
2021-09-12 22:50:32 +0200

    config(tailwind): jit mode, extanded colors, form plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  mode: 'jit',
   purge: {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    safelist: ['bg-red-500']
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "warm-gray": colors.warmGray
+      },
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
