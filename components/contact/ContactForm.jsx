fb05f20301fce9eb2293a1fcef4f8d4e79105952
Corentin THOMAS <corentin.thomas2@etu.univ-lorraine.fr>
2021-09-12 22:52:04 +0200

    feat: contact form/modal

---
+++ components/contact/ContactForm.jsx
@@ -0,0 +1,141 @@
+import { useTranslation } from "react-i18next";
+
+export default function ContactForm({ setOpen }) {
+  const { t } = useTranslation();
+  return (
+    <div className="px-6 py-10 sm:px-10 lg:col-span-2">
+      <h3 className="text-lg font-medium text-warm-gray-900">
+        {t("contactForm.contactme")}
+      </h3>
+      <form
+        method="POST"
+        className="grid grid-cols-1 mt-6 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+        data-netlify="true"
+        action="/?success"
+      >
+        <div>
+          <label
+            htmlFor="first-name"
+            className="block text-sm font-medium text-warm-gray-900"
+          >
+            {t("contactForm.firstname")}
+          </label>
+          <div className="mt-1">
+            <input
+              required
+              type="text"
+              name="first-name"
+              id="first-name"
+              autoComplete="given-name"
+              className="block w-full px-4 py-3 rounded-md shadow-sm text-warm-gray-900 focus:ring-indigo-500 focus:border-teal-500 border-warm-gray-300"
+            />
+          </div>
+        </div>
+        <div>
+          <label
+            htmlFor="last-name"
+            className="block text-sm font-medium text-warm-gray-900"
+          >
+            {t("contactForm.lastname")}
+          </label>
+          <div className="mt-1">
+            <input
+              required
+              type="text"
+              name="last-name"
+              id="last-name"
+              autoComplete="family-name"
+              className="block w-full px-4 py-3 rounded-md shadow-sm text-warm-gray-900 focus:ring-indigo-500 focus:border-teal-500 border-warm-gray-300"
+            />
+          </div>
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-warm-gray-900"
+          >
+            {t("contactForm.email")}
+          </label>
+          <div className="mt-1">
+            <input
+              required
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              className="block w-full px-4 py-3 rounded-md shadow-sm text-warm-gray-900 focus:ring-indigo-500 focus:border-teal-500 border-warm-gray-300"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-between">
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-warm-gray-900"
+            >
+              {t("contactForm.phone")}
+            </label>
+            <span id="phone-optional" className="text-sm text-warm-gray-500">
+              {t("contactForm.optional")}
+            </span>
+          </div>
+          <div className="mt-1">
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              autoComplete="tel"
+              className="block w-full px-4 py-3 rounded-md shadow-sm text-warm-gray-900 focus:ring-indigo-500 focus:border-teal-500 border-warm-gray-300"
+              aria-describedby="phone-optional"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="subject"
+            className="block text-sm font-medium text-warm-gray-900"
+          >
+            {t("contactForm.subject")}
+          </label>
+          <div className="mt-1">
+            <input
+              required
+              type="text"
+              name="subject"
+              id="subject"
+              className="block w-full px-4 py-3 rounded-md shadow-sm text-warm-gray-900 focus:ring-indigo-500 focus:border-teal-500 border-warm-gray-300"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <div className="flex justify-between">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-warm-gray-900"
+            >
+              {t("contactForm.message")}
+            </label>
+          </div>
+          <div className="mt-1">
+            <textarea
+              required
+              id="message"
+              name="message"
+              rows="4"
+              className="block w-full my-0 h-[131px] px-4 py-3 border rounded-md shadow-sm text-warm-gray-900 focus:ring-indigo-500 focus:border-teal-500 border-warm-gray-300"
+              aria-describedby="message-max"
+            ></textarea>
+          </div>
+        </div>
+        <div className="sm:col-span-2 sm:flex sm:justify-end">
+          <button
+            type="submit"
+            className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+          >
+            {t("contactForm.submit")}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ components/contact/SuccessContactNotification.jsx
@@ -0,0 +1,63 @@
+import { Fragment, useState, useEffect } from 'react'
+import { Transition } from '@headlessui/react'
+import { CheckCircleIcon } from '@heroicons/react/outline'
+import { XIcon } from '@heroicons/react/solid'
+import { useTranslation } from "react-i18next";
+
+export default function SuccessContactNotification() {
+  const [show, setShow] = useState(true)
+  const { t } = useTranslation();
+
+  useEffect(function() {
+    setTimeout(() => {
+      setShow(false)
+    }, 4000)
+  }, [show]);
+
+  return (
+    <>
+      <div
+        aria-live="assertive"
+        className="fixed inset-0 z-50 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start top-8"
+      >
+        <div className="flex flex-col items-center w-full space-y-4 sm:items-end">
+          <Transition
+            show={show}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-lg pointer-events-auto ring-1 ring-black ring-opacity-5">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <CheckCircleIcon className="w-6 h-6 text-green-400" aria-hidden="true" />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">{t('contactForm.success.title')}</p>
+                    <p className="mt-1 text-sm text-gray-500">{t('contactForm.success.message')}</p>
+                  </div>
+                  <div className="flex flex-shrink-0 ml-4">
+                    <button
+                      className="inline-flex text-gray-400 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      onClick={() => {
+                        setShow(false)
+                      }}
+                    >
+                      <span className="sr-only">{t('contactForm.success.close')}</span>
+                      <XIcon className="w-5 h-5" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    </>
+  )
+}
