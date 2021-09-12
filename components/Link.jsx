75ebccf374e35a98f6beac3fb62df20db1b3544a
Corentin THOMAS <corentin.thomas2@etu.univ-lorraine.fr>
2021-09-12 22:51:42 +0200

    feat: Link component

---
+++ components/Link.jsx
@@ -0,0 +1,20 @@
+import { ExternalLinkIcon } from "@heroicons/react/outline";
+
+export default function Link({ backgroundColor, color, Icon, text, alt, href }) {
+  return (
+    <a
+      style={{ backgroundColor, color }}
+      className="flex items-center justify-between w-full p-4 text-xl rounded-lg cursor-pointer hover:backdrop-opacity-80 transition duration-300 ease-in-out transform  hover:-translate-y-0.5 hover:scale-102"
+      href={href}
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+    >
+      <div className="inline-flex">
+        <Icon />
+        <span className="ml-3"> <span className="sr-only">{alt}: </span> {text}</span>
+      </div>
+
+      <ExternalLinkIcon className="w-6 h-6" />
+    </a>
+  );
+}
