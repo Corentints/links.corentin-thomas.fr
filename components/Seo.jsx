769f0cff6e12bd932a52acc9ba9d657551a6d3c7
Corentin THOMAS <corentin.thomas2@etu.univ-lorraine.fr>
2021-09-15 21:23:35 +0200

    fix(seo): correct image link card

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -7,14 +7,14 @@ export default function Seo() {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Corentin THOMAS | Links" />
       <meta property="og:description" content="Retrouvez tous les liens vers mes différents réseaux !" />
-      <meta property="og:image" content="card.png" />
+      <meta property="og:image" content="https://links.corentin-thomas.fr/card.png" />
 
       <meta name="twitter:card" content="summary" />
       <meta property="twitter:domain" content="links.corentin-thomas.fr" />
       <meta property="twitter:url" content="https://links.corentin-thomas.fr/" />
       <meta name="twitter:title" content="Corentin THOMAS | Links" />
       <meta name="twitter:description" content="Retrouvez tous les liens vers mes différents réseaux !" />
-      <meta name="twitter:image" content="card.png" />
+      <meta name="twitter:image" content="https://links.corentin-thomas.fr/card.png" />
     </>
   );
 }
